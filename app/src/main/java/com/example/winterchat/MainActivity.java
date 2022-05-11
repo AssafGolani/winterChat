4abe96d26680fe2cbfecdde3d43fb4249468b3e6
Assaf <1214assaf@gmail.com>
2022-05-11 18:59:06 +0300

    add signOut functionality modified signin functionality

--- app/src/main/java/com/example/winterchat/MainActivity.java
+++ app/src/main/java/com/example/winterchat/MainActivity.java
@@ -6,12 +6,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.FirebaseAuth;
@@ -23,6 +26,33 @@ public class MainActivity extends AppCompatActivity {
     private static final int REQ_CODE_ON_CREATE = 1;
     private static final int REQ_CODE_AFTER_SIGN_OUT = 2;
 
+    public void signIn(int requestCode){
+        Intent signInUp = AuthUI.getInstance().createSignInIntentBuilder().build();
+            /*
+            In order to open an activity using an Intent object:
+            1. startActivity
+            2. startActivityForResult
+             */
+        startActivityForResult(signInUp, requestCode);
+    }
+
+    /**
+     * Handle signOut procedure asynchronously
+     * if signOut is done asynchronously - it will happen sometime in the future
+     * Google Task Services API - signal completion
+     * Then and only then will we continue with the rest of the code in the method
+     * Use CompletionListener to get updated upon signout completion
+     */
+    private void signOut(){
+        Task<Void> signOutTask = AuthUI.getInstance().signOut(this);
+        signOutTask.addOnCompleteListener(task ->{
+            Toast.makeText(MainActivity.this,
+                    "Signed out successfully",
+                    Toast.LENGTH_SHORT).show();
+            signIn(REQ_CODE_AFTER_SIGN_OUT);
+        });
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,18 +62,36 @@ public class MainActivity extends AppCompatActivity {
          */
         if (FirebaseAuth.getInstance().getCurrentUser() == null){  //user is not signed-in
             //prompt user to sign-in/sign-up
-            Intent signInUp = AuthUI.getInstance().createSignInIntentBuilder().build();
-            /*
-            In order to open an activity using an Intent object:
-            1. startActivity
-            2. startActivityForResult
-             */
-            startActivityForResult(signInUp, REQ_CODE_ON_CREATE);
+            signIn(REQ_CODE_ON_CREATE);
         }
         else{ // user is signed in
             showDetails(true);
             enterChatRoom();
         }
+
+        FloatingActionButton button = (FloatingActionButton)findViewById(R.id.fab);
+        //TODO: push data to firebase realtime-db
+    }
+
+    /**
+     * Similar to setContentView invoked in onCreate method
+     * this method populates the menu-bar section of the screen
+     * with items declared in a menu resource xml file
+     * @param menu the reference to the menu section in runtime
+     * @return
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_activity_menu, menu);
+        // if app doesn't crash, return true
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        // only one menu item can be selected at a time
+        if (item.getItemId() == R.id.log_out_item) signOut();
+        return true;
     }
 
     /**
@@ -91,23 +139,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    /**
-     * Handle signOut procedure asynchronously
-     * if signOut is done asynchronously - it will happen sometime in the future
-     * Google Task Services API - signal completion
-     * Then and only then will we continue with the rest of the code in the method
-     * Use CompletionListener to get updated upon signout completion
-     */
-    private void signOut(){
-        Task<Void> signOutTask = AuthUI.getInstance().signOut(this);
-        signOutTask.addOnCompleteListener(new OnCompleteListener<Void>() {
 
-            @Override
-            public void onComplete(@NonNull Task<Void> task) {
-                Toast.makeText(MainActivity.this, "Signed out successfully", Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
 
     /**
      * This method opens a new activity representing 2 authenticated users that can chat
@@ -119,4 +151,8 @@ public class MainActivity extends AppCompatActivity {
     public void signInAfterSignOut(){
 
     }
+
+
+
+
 }
